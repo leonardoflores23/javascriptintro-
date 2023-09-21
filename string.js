9f39792680c3ad1b6a1f4d02d6f7f5d0caaf2f95
leonardo flores <lseflores01@gmail.com>
2023-09-20 22:56:57 -0600

    clase de javascript intro

---
+++ string.js
@@ -0,0 +1,8 @@
+let string ="hola clase de Ada School";
+// 0 1 2 3 4 5 6 ...
+
+console.log(string.length);
+console.log(string[2]);
+console.log(string[14]);
+console.log(string[7]);
+console.log(string.indexOf("de"));
---
+++ variables.js
@@ -0,0 +1,47 @@
+let nombre = "Leonardo Flores"; // string
+let nota = 49; //number
+let estadoInscrirto = false; // booleanos
+nota = 1;
+nombre = "Ada School"
+
+const nota2 = 100;
+//nota2 = 10; valor que no se puede cambiar
+
+//console.log(nombre);
+//console.log(nota);
+//console.log(estadoInscrirto);
+//console.log(nota2)
+
+//Ejercicios con operadores aritmeticos
+//let numero2 = 10;
+//2^2=2*2
+// 39^10 = 39*39*39*39
+//let resultado = numero1 + numero2;
+//console.log(resultado);
+
+let numero1 = 2;
+//numero1 = numero1 + 10
+console.log(numero1); //2
+numero1 += 10; //12
+console.log(numero1);
+let resultado2 = numero1 + 10; //12 + 10
+console.log(resultado2);
+
+
+//operadores de comparacion
+// = asignacion
+// == comparacion
+// === comparacion + tipo de dato
+
+//let valor1 = 3;
+//let valor2 = "3" 
+//let resultado = valor1 == valor2;
+//console.log(resultado);
+
+
+//let miEdad;
+//miEdad = 21
+//console.log(miEdad);
+//let miNombre
+//miNombre = "Leonardo Flores"
+//console.log(miNombre)
